4e2f50d66aec81bc22d6878c0b3d5eb2d7ecf579
Olaniyan Jide <olaniyanjide@hotmail.com>
2024-03-11 12:09:48 +0100

    First Commit

---
+++ eks/01-versions.tf
@@ -0,0 +1,14 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = var.aws_region
+}
